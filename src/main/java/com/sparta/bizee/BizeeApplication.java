5a19e14995ffed3c33653cf9f2c991cad9ea35c3
soyoung <soyoung.yang94@gmail.com>
2024-05-14 12:09:42 +0900

    initial commit

---
+++ src/main/java/com/sparta/bizee/BizeeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.bizee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/bizee/BizeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.bizee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
