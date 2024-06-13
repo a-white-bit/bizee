d4f4db6b6286b79a6c94456f6071aa0a9551cc45
soyoung <soyoung.yang94@gmail.com>
2024-06-13 09:14:14 +0900

    fix: schedule 엔터티 수정
    - schedule - comment 엔터티 간 연관관계 설정 변경: 연쇄삭제, 고아 삭제, 지연로딩 옵션

--- src/main/java/com/sparta/bizee/entity/Schedule.java
+++ src/main/java/com/sparta/bizee/entity/Schedule.java
@@ -1,9 +1,11 @@
 package com.sparta.bizee.entity;
 
 import com.sparta.bizee.dto.ScheduleRequestDto;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -46,7 +48,9 @@ public class Schedule extends Timestamped {
 
     // 1:N Comment 에 의해 양방향 설정
     // 단방향이어도 로직에 문제가 없는지 고려후 삭제
-    @OneToMany(mappedBy = "schedule")
+    // 외래키로 인해서 일정 삭제시 exception이 발생하고 삭제되지 않음 --> 해결법: 고아 엔티티 삭제 옵션 true 하기
+    // 항상 댓글 정보를 불러오지 않으므로 지연 로딩 설정 LAZY
+    @OneToMany(mappedBy = "schedule", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Comment> comments = new ArrayList<>();
 
     public Schedule(ScheduleRequestDto requestDto) {
